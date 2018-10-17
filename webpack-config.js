a97de3bf68b97243095b91b8387dbf9fe4199a00
piotr-s <piotr.slawinski1@gmail.com>
2018-10-17 18:57:39 +0200

    Modify webpack-config

--- webpack-config.js
+++ webpack-config.js
@@ -1,4 +1,12 @@
+const path = require("path");
+
 module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./public/javascripts"),
+    filename: "bundle.js"
+  },
   module: {
     rules: [
       {
